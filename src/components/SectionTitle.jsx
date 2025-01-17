b6e4cc1d61b939c7c87b1ff9a50ab92092dc26a9
nithilanoor <nithilanoorchowdhury@gmail.com>
2025-01-17 20:13:21 +0600

    Authentication Setup

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -6,7 +6,7 @@ const SectionTitle = ({ title, subtitle }) => {
             <h2 className="text-4xl font-serif md:text-5xl font-bold text-[#e0deda]">
                 {title}
             </h2>
-            <p className="text-lg uppercase text-[#3A3F00]">
+            <p className="text-sm md:text-lg uppercase text-[#3A3F00]">
                 --{subtitle}--
             </p>
         </div>
