47da5eab730512dce984b714a69609d1574f727e
nithilanoor <nithilanoorchowdhury@gmail.com>
2025-01-19 13:26:30 +0600

    Trending Products Section

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,10 +1,11 @@
+import { AiFillProduct } from "react-icons/ai";
 import { BiSolidHomeHeart } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 
 const ProductItem = ({ item }) => {
 
-    const { _id, name, image, tags, upvotes } = item;
+    const { _id, name, image, tags, upvotes, category } = item;
 
     return (
         <div className="card bg-base-100 shadow-xl border border-[#e1e2d7]">
@@ -14,15 +15,15 @@ const ProductItem = ({ item }) => {
                     alt="Products" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title text-[#3A3F00] font-semibold font-serif">
-                    <Link to={`/details/${_id}`}>{name}</Link>
-                    <div className="badge bg-indigo-200 text-white">Featured</div>
-                </h2>
+                <Link to={`/details/${_id}`}><h2 className="card-title text-[#3A3F00] font-semibold font-serif">
+                    <AiFillProduct />{name}
+                    <div className="badge bg-indigo-200 text-white text-xs">{category}</div>
+                </h2></Link>
             </div>
             <div className="card-actions justify-start mx-3">
                 {tags.map((tag) => <div key={tag} className="badge badge-outline">{tag}</div>)}
             </div>
-            <div className="card-actions justify-center my-6">
+            <div className="card-actions justify-start mx-3 my-6">
                 <button className="flex items-center gap-1 text-[#3A3F00] hover:text-indigo-300 border px-1 rounded-lg hover:border-indigo-100 border-[#e1e2d7]"><BiSolidHomeHeart /> {upvotes}</button>
             </div>
         </div>
--- src/pages/HomePage/Trending.jsx
+++ src/pages/HomePage/Trending.jsx
@@ -1,15 +1,34 @@
 import { IoIosArrowForward } from "react-icons/io";
 import SectionTitle from "../../components/SectionTitle";
 import { Link } from "react-router-dom";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { useEffect, useState } from "react";
+import ProductItem from "../../components/ProductItem";
 
 
 const Trending = () => {
+
+    const [product, setProduct] = useState([]);
+    const axiosPublic = useAxiosPublic();
+
+    useEffect(() => {
+        axiosPublic.get('/trending/products')
+            .then(res => {
+                setProduct(res.data);
+            })
+    }, [])
+
     return (
         <div>
             <div>
                 <SectionTitle title={'trending'} subtitle={"Discover what’s making waves in the world of tech"}></SectionTitle>
             </div>
-            <div className="mx-auto text-center my-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-8">
+                {
+                    product.map(item => <ProductItem key={item._id} item={item}></ProductItem>)
+                }
+            </div>
+            <div className="mx-auto text-center my-10">
                 <Link to="allProducts"><button className="btn hover:bg-[#F4F1EC] bg-[#8f9182] text-white hover:text-[#3A3F00] text-lg font-serif">
                     Show All Products <IoIosArrowForward />
                 </button></Link>
